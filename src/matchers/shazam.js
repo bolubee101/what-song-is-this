06009a17d39bde2e45d4950d1922bd9b31094e84
Lord Bee <akinwandeakinboluwarin@gmail.com>
2022-10-21 02:29:36 +0100

    integrated with audd.io as a fall back music matcher

--- src/matchers/shazam.js
+++ src/matchers/shazam.js
@@ -1,7 +1,37 @@
 // send the base64 data to the shazam api
 import axios from "axios";
 import convertToRaw from "../converters/rawConverter.js";
-const shazamRequest = async (file_path) => {
+import { saveSong } from "../models.js/songs.js";
+
+export const parseShazamResponse = async (song, in_reply_to_status_id_str) => {
+  try {
+    if (song?.matches?.length) {
+      const sections = song.track.sections;
+      const video_url_match = sections.find(
+        (section) => section.type === "VIDEO"
+      )?.youtubeurl?.actions[0]?.uri;
+
+      const formatted = {
+        song_name: song.track.title,
+        song_artist: song.track.subtitle,
+        song_url: video_url_match || song.track.url,
+      };
+
+      await saveSong({
+        in_reply_to_status_id_str,
+        match_payload: song,
+        source: "shazam",
+        ...formatted,
+      });
+      return formatted;
+    }
+    return null;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const shazamRequest = async (file_path, in_reply_to_status_id_str) => {
   return new Promise(async (resolve, reject) => {
     try {
       const options = {
@@ -12,10 +42,14 @@ const shazamRequest = async (file_path) => {
           "X-RapidAPI-Key": process.env.RAPID_API_KEY,
           "X-RapidAPI-Host": "shazam.p.rapidapi.com",
         },
-        data: await convertToRaw(file_path),
+        data: await convertToRaw({ file_path }),
       };
       const resp = await axios(options);
-      return resolve(resp.data);
+      const response = await parseShazamResponse(
+        resp.data,
+        in_reply_to_status_id_str
+      );
+      return resolve(response);
     } catch (error) {
       return reject(error);
     }
--- src/utils/generateReply.js
+++ src/utils/generateReply.js
@@ -1,16 +1,13 @@
 // function that takes in an song object and a username and returns a reply string
 
-export const generateReplyToVideoTag = (song, username, new_match = false) => {
+export const generateReplyToVideoTag = (song, username) => {
   if (!song?.song_name && !song?.track?.title) {
     return `I have failed you @${username}, for I could not find the song in this video.`;
   }
-  const song_name = !new_match ? song.song_name : song.track.title;
-  const song_artist = !new_match ? song.song_artist : song.track.subtitle;
-  const song_url = !new_match ? song.song_url : song.track.url;
-  const video_url = song?.track?.sections?.find(
-    (section) => section.type === "VIDEO"
-  )?.youtubeurl?.actions[0]?.uri;
+  const song_name = song.song_name;
+  const song_artist = song.song_artist;
+  const song_url = song.song_url;
 
   return `My lord @${username} I think this song is "${song_name}" by "${song_artist}" You can check out the song here: 
-    ${video_url || song_url}`;
+    ${song_url}`;
 };
