e7e49ab459e8dbd05d7792f41cbb3114766a65fd
Lord Bee <akinwandeakinboluwarin@gmail.com>
2022-12-25 15:50:55 +0100

    open api

--- src/utils/gpt3.js
+++ src/utils/gpt3.js
@@ -1,16 +1,21 @@
-import openai from "openai";
+import axios from "axios";
+const apiKey = process.env.OPENAI_TOKEN;
+const GPT3_API_URL = "https://api.openai.com/v1/completions";
 
-export async function generateText(prompt) {
-  openai.init({
-    apiKey: process.env.OPENAPI_TOKEN,
-  });
+export async function generateText(prompt, model = "text-babbage-001") {
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${apiKey}`,
+  };
 
-  const response = await openai.completion.create({
-    engine: "davinci",
+  const data = {
+    model,
     prompt: prompt,
     temperature: 0.5,
     max_tokens: 2048,
-  });
+    top_p: 1,
+  };
 
-  return response.text;
+  const response = await axios.post(GPT3_API_URL, data, { headers: headers });
+  return response.data.choices[0].text;
 }
